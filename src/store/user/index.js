9bab0017aebf79cd613a621e0584791b2bc2766e
aofdev <aof.dev@gmail.com>
2017-09-22 19:13:46 +0700

    code modules vuex shared & user

---
+++ src/store/user/index.js
@@ -0,0 +1,82 @@
+import * as firebase from 'firebase'
+
+export default {
+  state: {
+    user: null
+  },
+  mutations: {
+    setUser (state, payload) {
+      state.user = payload
+    }
+  },
+  actions: {
+    signUserUp ({commit}, payload) {
+      commit('setLoading', true)
+      commit('clearError')
+      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+        .then(
+          user => {
+            commit('setLoading', false)
+            const newUser = {
+              id: user.uid,
+              name: user.displayName,
+              email: user.email,
+              photoUrl: user.photoURL,
+              fbKeys: {}
+            }
+            commit('setUser', newUser)
+          }
+        )
+        .catch(
+          error => {
+            commit('setLoading', false)
+            commit('setError', error)
+            console.log(error)
+          }
+        )
+    },
+    signUserIn ({commit}, payload) {
+      commit('setLoading', true)
+      commit('clearError')
+      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+        .then(
+          user => {
+            commit('setLoading', false)
+            const newUser = {
+              id: user.uid,
+              name: user.displayName,
+              email: user.email,
+              photoUrl: user.photoURL,
+              fbKeys: {}
+            }
+            commit('setUser', newUser)
+          }
+        )
+        .catch(
+          error => {
+            commit('setLoading', false)
+            commit('setError', error)
+            console.log(error)
+          }
+        )
+    },
+    autoSignIn ({commit}, payload) {
+      commit('setUser', {
+        id: payload.uid,
+        name: payload.displayName,
+        email: payload.email,
+        photoUrl: payload.photoURL,
+        fbKeys: {}
+      })
+    },
+    logout ({commit}) {
+      firebase.auth().signOut()
+      commit('setUser', null)
+    }
+  },
+  getters: {
+    user (state) {
+      return state.user
+    }
+  }
+}
